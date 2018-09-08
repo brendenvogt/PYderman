e7cf35dcde8732dfa2e1f1d2c55425c69f88d5d2
BrendenVogt <bvogtdev@gmail.com>
2018-09-08 16:17:53 -0700

    enhanced parsing of urls. added helper methods.

--- PyterParker.py
+++ PyterParker.py
@@ -1,5 +1,10 @@
 import requests
+
 import urllib.request
+from urllib.parse import urljoin
+from urllib.parse import urlsplit, urlunsplit
+
+
 from bs4 import BeautifulSoup
 from datetime import datetime
 import os
@@ -7,10 +12,9 @@ import uuid
 import shutil
 
 
-
 class PyterParker():
 
-    def __init__(self, url, name, req="urllib", crawlDepth=0):
+    def __init__(self, name, req="urllib", crawlDepth=0):
         self.name = name or self._getDateTime()
         self.url = url
         self.crawlDepth = crawlDepth
@@ -22,8 +26,8 @@ class PyterParker():
         self._imgDir = "img"
         self._graphDir = "graph"
 
-    def run(self):
-        html = self.grab(self.url)
+    def run(self, url):
+        html = self.grab(url)
         self.parse(url, html)
 
     def grab(self, url):
@@ -35,23 +39,33 @@ class PyterParker():
         return html
 
     def parse(self, url, content):
+        # soup parsing
         soup = BeautifulSoup(content, 'html.parser')
-        self.parseImgs(url, soup)
-        self.parseUrls(url, soup)
+        
+        # url parsing
+        urls = self.parseUrls(self._getBase(url), soup)
+        self._printAll(urls)
+        
+        # imgs 
+        imgs = self.parseImgs(soup)
+        self._printAll(imgs)
 
     # Parse Methods
 
-    def parseImgs(self, url, soup):
+    def parseImgs(self, soup):
         imgs = soup.find_all("img")
-        for img in imgs:
-            imgUrl = img.get("src") or img.get("data-lazyload")
-            self.imgUrls.append((url, imgUrl))
-
-    def parseUrls(self, url, soup):
+        return [img.get("src") or img.get("data-lazyload") for img in imgs]
+        
+    def parseUrls(self, urlbase, soup):
         links = soup.find_all("a")
+        cleaned = []
         for link in links:
-            linkUrl = link.get("href")
-            self.subUrls.append((url, linkUrl))
+            link = link.get("href")
+            if link and self._isRelative(link):
+                link = urljoin(url, link[1:])
+            if link and link != "":
+                cleaned.append(link)
+        return cleaned
 
     # Save Methods
 
@@ -78,19 +92,43 @@ class PyterParker():
 
     # Helper
 
+    def _getBase(self, url):
+        split_url = urlsplit(url)
+        clean_path = "".join(split_url.path.rpartition("/")[:-1])
+        clean_url = urlunsplit((split_url.scheme, split_url.netloc, "", "", ""))
+        return clean_url
+
+    def _isRelative(self, url):
+        return url[0] == "#" or url[0] == "/"
+
     def _getDateTime(self):
         return datetime.utcnow().strftime("%d-%m-%y-%H-%M-%S")
 
+    def _printAll(self, items):
+        for i in items:
+            print(i)
+
+    def _isValid(self, url):
+        try:
+            if self.req == "urllib":
+                r = urllib.request.urlopen(url)
+                return r.getcode() == 200
+            if self.req == "requests":
+                r = requests.get(url)
+                return r.status_code == 200
+            return False
+        except:
+            return False
 
 if __name__ == "__main__":
     print("My spider senses are tingling")
 
     # url = "https://www.amazon.com/TCL-49S405-49-Inch-Ultra-Smart/dp/B01MYGISTO/ref=sr_1_1_sspa?s=tv&ie=UTF8&qid=1536346649&sr=1-1-spons&keywords=tv&psc=1"
     url = "https://www.iherb.com/pr/p/11242"
-    parser = PyterParker(url=url, name=None, req="requests", crawlDepth=2)
-    parser.run()
+    parser = PyterParker(name=None, req="requests", crawlDepth=2)
+    parser.run(url)
 
-    ##IMAGES
-    parser.saveImages()	
-    ##GRAPH
-    parser.saveGraph()
+    # ##IMAGES
+    # parser.saveImages()	
+    # ##GRAPH
+    # parser.saveGraph()
